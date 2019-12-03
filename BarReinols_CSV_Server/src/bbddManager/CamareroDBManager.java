11e36f947de257452d95ab939b8af79c54ab3c8c
erik-cf <erikcf95@gmail.com>
2019-12-03 16:48:20 +0100

    Encriptacio i stored procedure acabat

--- BarReinols_CSV_Server/src/bbddManager/CamareroDBManager.java
+++ BarReinols_CSV_Server/src/bbddManager/CamareroDBManager.java
@@ -2,20 +2,23 @@ package bbddManager;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import com.example.barreinolds.Camarero;
 import com.example.barreinolds.Main;
+import java.sql.CallableStatement;
 
-public class CamareroDBManager extends ConnectionManager{
-	
+public class CamareroDBManager extends ConnectionManager {
+
 	public static void insertCamarero(Camarero c) throws SQLException, IOException {
-		String insert = "INSERT INTO camareros(nombre, username, password, image_blob) VALUES ('" + c.getNombre() + "', '" + c.getUsername() + "', '" + c.getPassword() + "', ?)";
-		PreparedStatement pstmnt = getConnection().prepareStatement(insert);
+		String procedure = "{CALL insert_employee(?, ?, ?, ?)}";
+		CallableStatement cstmnt = getConnection().prepareCall(procedure);
+		cstmnt.setString(1, c.getNombre());
+		cstmnt.setString(2, c.getUsername());
+		cstmnt.setString(3, c.getPassword());
 		ByteArrayInputStream bais = new ByteArrayInputStream(c.getImageEmployee());
-		pstmnt.setBinaryStream(1, bais);
-		pstmnt.executeUpdate();
+		cstmnt.setBinaryStream(4, bais);
+		cstmnt.execute();
 		Main.setCamareros(InitBDManager.getCamareros());
 	}
 
